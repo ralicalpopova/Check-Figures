cceb0e079e2444916ebf3935fdbee204e7f24284
ralicalpopova <ralicalpopova@gmail.com>
2021-05-26 17:15:35 +0300

    Update CheckFigure.h

--- CheckFigure.h
+++ CheckFigure.h
@@ -10,9 +10,9 @@ class CheckFigures
 	int ROW_MAX = 0;
 	int COL_MAX = 0;
 
-	void goDeep(const vector< vector <int>> matrix, vector< vector <bool>> &checked, int row, int col, short int ComesFrom);
+	void goDeep(const vector< vector <int>> &matrix, vector< vector <bool>> &checked, int row, int col, short int ComesFrom);
 
-	void checkMatrix(const vector< vector <int>> matrix, vector< vector <bool>> &checked, int row, int col);
+	void checkMatrix(const vector< vector <int>> &matrix, vector< vector <bool>> &checked, int row, int col);
 
 public:
 	void check(const vector<vector<int>> matrix);
